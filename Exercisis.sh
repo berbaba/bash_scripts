23067900e783b5be2fc3ebdff358092c7e91db45
Bernat <bernatbba@gmail.com>
2020-09-30 23:56:15 +0200

    Exerc.1 working

--- Exercisis.sh
+++ Exercisis.sh
@@ -3,12 +3,11 @@
 # Exercisi 1
 # Llegir valor enter i comprovar rang correcte o missatge error. Per defecte assumir 0.
 
-if [ ! $1 ]
+nota=${1}
+if [ ! $nota ]
 then
-  echo "Cal donar l'argument: Nota del 0 al 10. Per defecte s'assumeix 0"
-  # UTILITZAR EXPANSIÓ DE VARIABLES
-  echo "Nota: " $1
-elif [ $1 -gt -1 ]  && [ $1 -lt 11 ]
+  echo "Cal donar l'argument: Nota del 0 al 10. (Per defecte s'assumeix nota = ${nota:= 0 })"
+elif [ ${nota} -gt -1 ]  && [ ${nota} -lt 11 ]
 then
   echo "Nota: "$1
 else
