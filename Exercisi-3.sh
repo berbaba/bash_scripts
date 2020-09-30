6a5ab9dcf732da88b8913601065c54f6fab267b4
Bernat <bernatbba@gmail.com>
2020-10-01 00:51:37 +0200

    Exerc-3

---
+++ Exercisi-3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Exercisi 3
+# Llegir seqüència de línies entrant per tubería amb cat fitxer
+# Dividir línies en paraules i eliminar signes puntuació
+# anar emmagatzemant paraules i anar imprimint per eixida
+# la quantitat d'ocurrències de cada paraula, ordenades per
+# nombre d'ocurrències decrecreixent
