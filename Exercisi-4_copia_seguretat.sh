5b00c379a33dffb57a91b977c3783ec6ed5fa461
Bernat <bernatbba@gmail.com>
2020-10-07 02:08:12 +0200

    Exerc-5

--- Exercisi-4_copia_seguretat.sh
+++ Exercisi-4_copia_seguretat.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Add description
+
 if ( ! /tmp/tmp_home 2> /dev/null )
 then
   echo "no existeix"
 fi
+declare -a v
+i=0
+ls $HOME | while read line
+do
+  for item in line
+    do
+      v[$i]=$line
+      echo ${v[$i]}
+      let i=${i}+1
+  done
+done
+echo ${v[5]}
---
+++ Exercisi-5_usuaris_processos_en_execucio.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Imprimir llistat usuaris amb processos en execució i num processos
+# Suma del consum actual de CPU i memòria (en %)
+
+# • Cantidad de procesos “bash” en ejecución:
+# $ ps aux | grep “bash” | grep -v “grep” | wc –l
+
+
+tot_proc=`ps aux | wc -l`  # Total procesess
+# tot_proc=$(ps aux | wc -l) # Same as open accent (inverse quotation marks)
+echo "Processos totals: "$tot_proc
+
+usu=`ps aux | cut -f 1 -d " " | sort | uniq`  # All process users
+echo "Usuaris: " $usu
+
+for us in $usu
+do
+  echo $us
+done
+# echo `ps aux | grep $us | cut -f 1 -d " "`
