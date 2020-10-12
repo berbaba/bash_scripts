9c0e702f59b58ad56a2b6cd49e8a396bbb89ec78
Bernat <bernatbba@gmail.com>
2020-10-12 19:57:55 +0200

    Exerc-4,5 not working as intended yet

--- Exercisi-5_usuaris_processos_en_execucio.sh
+++ Exercisi-5_usuaris_processos_en_execucio.sh
@@ -3,9 +3,9 @@
 # Imprimir llistat usuaris amb processos en execució i num processos
 # Suma del consum actual de CPU i memòria (en %)
 
-# • Cantidad de procesos “bash” en ejecución:
-# $ ps aux | grep “bash” | grep -v “grep” | wc –l
-
+# Sugerencias: Se sugiere utilizar la orden ps para averiguar la
+# información de base. Utilice la orden “bc” para sumar los
+# consumos de CPU y memoria, ya que son números reales
 
 tot_proc=`ps aux | wc -l`  # Total procesess
 # tot_proc=$(ps aux | wc -l) # Same as open accent (inverse quotation marks)
@@ -14,10 +14,18 @@ echo "Processos totals: "$tot_proc
 usu=`ps aux | cut -f 1 -d " " | sort | uniq`  # All process users
 echo "Usuaris: " $usu
 
-# for us in $usu
-# do
-#   echo $us
-# done
-
+y=0
+# euser
 # col 3 cpu, col 4 mem
-ps -eo euser,pcpu
+ps -eo "pcpu,start_time" | grep ":" | cut -c -4 | while read line; do
+  # echo $line
+  y=$(echo "$line + $y" | bc)
+  echo $y a
+done
+echo $y b
+
+
+# dubte
+
+  # • Cantidad de procesos “bash” en ejecución:
+  # $ ps aux | grep “bash” | grep -v “grep” | wc –l
